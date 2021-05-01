1053af640f6fa0e886c6d117a49dca3d62e1f530
Rainbow-Engine <yazaki.tatsuya.0813.03@gmail.com>
2021-05-01 16:52:05 +0900

    Test Commit

---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string dataSource, initCatalog, userId, passWord, connectionString;
+            dataSource = "150-95-209-40";
+            initCatalog = "master";
+            userId = "SA";
+            passWord = "Tatsuya0813";
+
+            connectionString = @"Data Source="+ dataSource + ";Initial Catalog=" +
+                initCatalog + ";User ID=" + userId + ";Password=" + passWord;
+            SqlConnection cnn = new SqlConnection(connectionString);
+            cnn.Open();
+            MessageBox.Show("DBコネクションを確立しました");
+        }
+    }
+}
